3f39bca9e6f526d421189207b58da36225abb43d
Gus Liakos <Gliakos@cnm.edu>
2018-04-30 08:35:29 -0600

    first class

--- Vehicle.php
+++ Vehicle.php
@@ -5,3 +5,40 @@
  * Date: 4/30/2018
  * Time: 8:27 AM
  */
+
+class Vehicle {
+
+	protected  $licensePlate;
+
+	/**
+	 * @return mixed
+	 */
+	public function getLicensePlate() {
+		return $this->licensePlate;
+	}
+
+	/**
+	 * @param mixed $licensePlate
+	 */
+	public function setLicensePlate($licensePlate) {
+		$this->licensePlate = $licensePlate;
+	}
+	public function __construct() {
+//	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+//			$exceptionType = get_class($exception);
+//			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+
+		echo "try and catch error/exception statements"
+}
+}
+
+class NissanAltima extends Vehicle {
+	protected $belongsToMarlon;
+
+	public
+}
+
+class HorseAndBuggy extends Vehicle {
+	protected numHorses;
+}
+
